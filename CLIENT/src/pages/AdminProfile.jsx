0c10b68d91a99b5debfba55be1b0e4c3700ec018
רחלי יצחקי <yracheli559@gmail.com>
2024-07-10 03:10:40 +0300

    start adminProfil

--- CLIENT/src/pages/AdminProfile.jsx
+++ CLIENT/src/pages/AdminProfile.jsx
@@ -1,5 +1,10 @@
 import React, { useState, useContext, useEffect } from 'react';
-import { TutorsContext, FilterContext } from '../App';
+import { UserContext,TutorsContext, FilterContext } from '../App';
+import { serverRequests } from '../Api';
+import { useNavigate } from 'react-router-dom';
+import '../css/studentProfile.css';
+import { sub } from 'date-fns';
+import { FaTrash, FaPlus } from 'react-icons/fa';
 
 
 const AdminProfile=()=>{
